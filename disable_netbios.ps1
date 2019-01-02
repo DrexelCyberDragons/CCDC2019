f8c82bbcb6202bc29e33799dc19c6a5b9ae53b09
DrexelCyberDragons <36054355+DrexelCyberDragons@users.noreply.github.com>
2019-01-02 18:02:11 -0500

    Delete disable_netbios.ps1

--- disable_netbios.ps1
+++
@@ -1,3 +0,0 @@
-$key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-Get-ChildItem $key |
-foreach { Set-ItemProperty -Path "$key\$($_.pschildname)" -Name NetbiosOptions -Value 2 -Verbose}
