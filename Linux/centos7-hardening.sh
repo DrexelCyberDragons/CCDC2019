5a5e3837bf6108f4550868d26b68755edb9bdc22
Alfonzo DeSantis <Alfonzo@Alfonzos-MacBook-Pro-4.local>
2019-01-03 17:09:32 -0500

    Linux Scripts added

---
+++ Linux/centos7-hardening.sh
@@ -0,0 +1,10 @@
+### CentOS
+
+## Add user to wheel group (sudoers)
+#usermod -aG wheel user
+
+## Removing root account
+#sudo passwd -ld root
+
+## Install ModSecurity
+sudo yum install mod_security mod_security_crs -y
---
+++ Linux/ubuntu18-hardening.sh
@@ -0,0 +1,47 @@
+###Ubuntu18
+
+##Notes
+#ssh -t
+#rm -rf .bashrc .bashprofile .profile
+
+##Installing and Activating AppArmor
+sudo apt-get upgrade && apt-get update -yuf
+sudo apt-get install apparmor-utils apparmor-profiles apparmor-profiles-extra -y
+sudo systemctl enable apparmor
+sudo systemctl start apparmor
+sudo aa-enforce /etc/apparmor.d/*
+
+##Installing and Activating ModSecurity
+sudo apt-get install libapache2-mod-security2 -y
+##Check if ModSecurity success (security2_modue (shared))
+#sudo apachectl -M | grep --color security2
+sudo mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+##Reload Apache2
+#sudo service apache2 start
+#sudo service apache2 restart
+sudo service apache2 reload
+##Turning on SecRuleEngine
+sudo sed -i "s/SecRuleEngine DetectionOnly/SecRuleEngine On/" /etc/modsecurity/modsecurity.conf
+##Replace ModSecurity folder with OWASP's modsecurity config
+
+sudo mv /usr/share/modsecurity-crs /usr/share/modsecurity-crs.bk
+sudo git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git /usr/share/modsecurity-crs
+sudo cp /usr/share/modsecurity-crs/crs-setup.conf.example /usr/share/modsecurity-crs/crs-setup.conf
+##Clobber ModSecurity Default conf with updated conf
+sudo mv security2patch.conf /etc/apache2/mods-enabled/security2.conf
+sudo systemctl restart apache2
+
+##Patch sshd_config
+
+sudo diff sshd_config /etc/ssh/sshd_config > sshd_config.patch
+sudo patch -l /etc/ssh/sshd_config sshd_config.patch
+
+##Check accounts for Empty passwords
+sudo cat /etc/shadow | awk -F: '($2==""){print $1}' > ~/emptypasswords.txt
+
+##Disable IPv6
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -p
+
+##Patch Network to
