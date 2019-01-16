1e7da464e1e4d200a5a1ed1758b2e7525d4eef86
Nick D <nick@dundermifflin.com>
2019-01-16 17:51:14 -0500

    Update agent install

--- IR/OSSEC/agent-install.sh
+++ IR/OSSEC/agent-install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#git clone [CyberDragonsGithub]
 wget https://github.com/ossec/ossec-hids/archive/3.1.0.tar.gz
 tar -xvzf 3.1.0.tar.gz
-#mv [CyberDragonsGithub]/autofill-conf ossec-hids-3.1.0/etc
+cp preloaded-vars.conf ossec-hids-3.1.0/etc
 sh ossec-hids-3.1.0/install.sh
 
 /var/ossec/bin/manage_agents
