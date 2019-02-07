5de49771135372c8c9587eafd4a1f94f1c05984e
Colbert Zhu <cyberdragonsdrexel@gmail.com>
2019-02-07 00:19:56 -0500

    IR scripts

--- IR/Get-SysmonLogs.ps1
+++ IR/Get-SysmonLogs.ps1
@@ -1,51 +1,96 @@
-﻿# . .\Get-SysmonLogs.ps1 -ComputerName 192.168.9.10 -Credential Administrator
+﻿# .\Get-SysmonLogs.ps1 -HostsFile .\hosts.txt -Loop
+# Log File Format - [HOST]:[DOMAIN]:[USERNAME]:[PASSWORD]
+# By default, the program retrieves the Sysmon logs and saves them as a powershell object in xml format
 
-# Optional parameters to get events from remote host
 param (
-    [Parameter(Position = 0, Mandatory = $False)]
-    [string]
-    $ComputerName,
-    [Parameter(Position = 1, Mandatory = $False)]
-    [System.Management.Automation.PSCredential]
-    $Credential
+    [Parameter(Mandatory=$true)][string]$HostsFile,
+    [Parameter(Mandatory=$false)][Switch]$SysmonView,
+    [Parameter(Mandatory=$false)][Switch]$Loop
 )
 
-# Get-WinEvent Parameters
-$WinEventParameters = @{ 'Oldest' = $True }
-if($ComputerName) { $WinEventParameters['ComputerName'] = $ComputerName }
-if($Credential) { $WinEventParameters['Credential'] = $Credential }
+# Read config file containing list of hosts and credentials
+$HostFileData = Get-Content $HostsFile
 
-# FilterHashtable Argument Array
-$WinEventFilters = @{ 'LogName' = 'Microsoft-Windows-Sysmon/Operational' }
+# For each host in the file, retrieve Sysmon logs
+do {
+    foreach ($line in $HostFileData) {
+        $StartTime = $(get-date)
 
-# Check if events were already imported
-if($Events) {
-    $DATE = $Events | Select -Last 1 | %{ $_.TimeCreated }
-    $DATE
-    $WinEventFilters['StartTime'] = $DATE
-}
-else {
-    echo "No log history..."
-    $SplitEvents = @()
-}
+        $Events = $null
+        $SplitEvents = @()
 
-$WinEventParameters['FilterHashtable'] = $WinEventFilters
+        # Split fields into values
+        $line = $line -split (":")
+        $Computer = $line[0]
+        $Domain = $line[1]
+        $Username = $line[2]
+        $Password = $line[3]
 
-$Events = Get-WinEvent @WinEventParameters -MaxEvents 500
+        Write-Host ----------$Computer----------
+    
+        # Generate PsCredential object
+        $SecureStringPassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+        $PSCredentialUsername = $Domain + "\" + $Username
+        echo $PSCredentialUsername
+        $Credential = New-Object System.Management.Automation.PSCredential($PSCredentialUsername, $SecureStringPassword)
+        
+        # Get-WinEvent Parameters
+        $LogFilename = $Computer + "_Sysmon_PS.xml"
+        $WinEventParameters = @{'Oldest' = $True}
+        $WinEventParameters['ComputerName'] = $Computer
+        $WinEventParameters['Credential'] = $Cred
 
-$Events | ForEach-Object {
-  $Lines = $_.Message –split [System.Environment]::NewLine
-  $Id = $_.Id
-  $TimeCreated = $_.TimeCreated
-  $EventName = $Lines[0] –replace ‘:’
-  $Event = New-Object PSObject
-  $Event | Add-Member -MemberType ‘NoteProperty’ –Name ‘Id’ –Value $Id
-  $Event | Add-Member –MemberType ‘NoteProperty’ –Name ‘EventName’ –Value $EventName
-  $Event | Add-Member –MemberType ‘NoteProperty’ –Name ‘TimeCreated’ –Value $TimeCreated
-  $Lines | Select-Object –Skip 1 | ForEach-Object {
-    $PropName,$PropValue = $_ -split ‘: ’
-    $PropName = $PropName –replace ‘ ’
-    Add-Member –InputObject $Event –MemberType NoteProperty –Name $PropName –Value $PropValue
-  }
-  $SplitEvents += $Event
-}
+        # FilterHashtable Arguments
+        $WinEventFilters = @{ 'LogName' = 'Microsoft-Windows-Sysmon/Operational' }
+
+        if(Test-Path $LogFilename) {
+            $SplitEvents = Import-Clixml -Path $LogFilename
+            $DATE = $SplitEvents | select -last 1 | %{$_.TimeCreated} # gets the timestamp of the most recent log in the xml file
+            Write-Host Latest Log: $DATE
+            $WinEventFilters['StartTime'] = $DATE
+        }
+        else {
+            Write-Host No log history...
+        }
+        
+        # attach FilterHashtable argument
+        $WinEventParameters['FilterHashtable'] = $WinEventFilters
+
+        $Events = Get-WinEvent @WinEventParameters -MaxEvents 500
+        
+        # Thank you Matt...
+        $Events | ForEach-Object {
+                $Lines = $_.Message –split [System.Environment]::NewLine
+                $Id = $_.Id
+                $TimeCreated = $_.TimeCreated
+                $EventName = $Lines[0] –replace ‘:’
+                $Event = New-Object PSObject
+                $Event | Add-Member -MemberType ‘NoteProperty’ –Name ‘Id’ –Value $Id
+                $Event | Add-Member -MemberType 'NoteProperty' -Name 'TimeCreated' -Value $TimeCreated
+                $Event | Add-Member –MemberType ‘NoteProperty’ –Name ‘EventName’ –Value $EventName
+                $Lines | Select-Object –Skip 1 | ForEach-Object {
+                $PropName,$PropValue = $_ -split ‘: ’
+                $PropName = $PropName –replace ‘ ’
+                Add-Member –InputObject $Event –MemberType NoteProperty –Name $PropName –Value $PropValue
+            }
+            $SplitEvents += $Event
+        }
+
+        Write-Host Pulled $Events.count events
+
+        Export-Clixml -InputObject $SplitEvents $LogFilename
+
+        $ElapsedTime = $(get-date) - $StartTime
+        $TotalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
+        Write-Host Log Retrieval Time: $TotalTime
+    
+        if($SysmonView) {
+            $LogFilename = $Computer + "_SysmonView.xml"
+            $WevtutilCmdString = "WEVTUtil query-events 'Microsoft-Windows-Sysmon/Operational' /r:" + $Computer + " /u:" + ($Domain+"\"+$Username) + " /p:" + $Password + " /format:xml /e:sysmonview > " + $LogFilename
+            echo $WevtutilCmdString
+            Invoke-Expression $WevtutilCmdString
+        }
+    }
+
+    Start-Sleep (5)  # this is in seconds
+} while($Loop)
