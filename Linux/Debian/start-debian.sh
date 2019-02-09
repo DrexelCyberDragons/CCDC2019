16de66cafe9b4516713a3b41dd843d1326924552
Alfonzo DeSantis <cyberdragonsdrexel@gmail.com>
2019-02-09 15:24:25 -0500

    script alterations

--- Linux/Debian/start-debian.sh
+++ Linux/Debian/start-debian.sh
@@ -1,17 +1,6 @@
-### Starter script
-
-## host scripts on webserver - curl and pipe to bash
-## ansible wrapper script to create backups - execute scripts
-##		pull back files and delete files on multiple hosts at once.
-
-
-### Wipe /etc/skel
-
+sudo systemctl stop sshd
 rm -rf /etc/skel
 mv ../config/skel /etc/
-
-### Create sudoer account & disable root
-
 useradd alfonzo -s /bin/bash
 echo -e "dustyspicy50\ndustyspicy50" | passwd alfonzo
 useradd sam -s /bin/bash
@@ -20,18 +9,12 @@ useradd colbert -s /bin/bash
 echo -e "openframe32\nopenframe32" | passwd colbert
 useradd nick -s /bin/bash
 echo -e "grassseize57\ngrassseize57" | passwd nick
-
 usermod -aG sudo alfonzo
 usermod -aG sudo sam
 usermod -aG sudo colbert
 usermod -aG sudo nick
 usermod -aG sudo root
-
 sudo passwd -ld root
-
-#sudo passwd -aS | grep " P \| NP " > passenabled.txt
-
-### Password Changes
 
 for i in $( cat /etc/passwd | awk -F: '$3 > 999 {print $1}' ); do
 	if [ "$i" = "alfonzo" ] ; then
@@ -48,8 +31,6 @@ for i in $( cat /etc/passwd | awk -F: '$3 > 999 {print $1}' ); do
 		echo -e "cyberdragons\ncyberdragons" | passwd $i
 	fi
 done
-
-### Remove sudoers
 
 grep '^sudo:.*$' /etc/group | cut -d: -f4 | sed "s/,/\n/g" > sudo.bk
 
@@ -75,9 +56,6 @@ sudo chown root:sudo /bin/su
 sudo chmod 754 /bin/su
 sudo chmod u+s /bin/su
 
-#sudo dpkg-statoverride --update --add root sudo 4750 /bin/su
-### No login
-
 cat /etc/passwd | awk -F: '$7 != "/usr/sbin/nologin" {print $1}' > bash.bk
 
 for i in $( cat /etc/passwd | awk -F: '$7 != "/usr/sbin/nologin" {print $1}' ); do
@@ -98,11 +76,6 @@ for i in $( cat /etc/passwd | awk -F: '$7 != "/usr/sbin/nologin" {print $1}' ); 
   fi
 done
 
-# Disable password auth -> key auth to users and root user
-# give nagios a key as well
-
-### Tools
-
-#sudo apt-get upgrade -y
-#sudo apt-get update -y
-#sudo apt-get install vim -y
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >>  /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
