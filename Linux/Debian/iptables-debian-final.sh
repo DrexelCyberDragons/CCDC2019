916033bbd6c5babd059f455b900e9ebee00e439f
Sampriti Panda <sampdingo@hotmail.com>
2019-02-10 17:59:13 -0500

    Add compressed iptables

---
+++ Linux/Debian/iptables-debian-final.sh
@@ -0,0 +1,17 @@
+iptables-save > old-iptables.bk
+iptables -F
+
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+iptables -A INPUT -p tcp --dport 2049 -j ACCEPT
+iptables -A INPUT -p tcp --dport 111 -j ACCEPT
+
+iptables -A INPUT -j DROP
+
+iptables -I INPUT 5 -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+iptables -I OUTPUT 5 -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+
+iptables-save > new-iptables.bk
+iptables-save > /etc/iptables/rules.v4
